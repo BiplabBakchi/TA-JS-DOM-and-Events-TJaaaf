b81872e708dfaded0a7a9d35c74d0fa31ba0876e
Biplab Bakchi <biplabbakchi3@gmail.com>
2022-08-01 14:36:50 +0530

    Disco Grid

---
+++ block-BJaael/code/script.js
@@ -0,0 +1,23 @@
+let bigBox = document.querySelector('.container');
+let box = document.querySelectorAll('.box');
+
+
+
+function generateRandomColor() {
+    let hexCharacters = ['0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f']
+    let color = '#';
+    for(let i = 0; i < 6; i++) {
+        let randomNumber = Math.floor(Math.random() * 16);
+        color = color + hexCharacters[randomNumber];
+    }
+    return color;
+}
+
+function handlerMouseMove() {
+    let randomColor = generateRandomColor();
+    box.forEach(color => {
+        color.style.backgroundColor = randomColor;
+    });
+}
+bigBox.addEventListener('mousemove', handlerMouseMove);
+
