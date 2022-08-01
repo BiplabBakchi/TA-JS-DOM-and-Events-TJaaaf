49ddadc92814e3af1a62dc51054ca5ca1b1a0d4c
Biplab Bakchi <biplabbakchi3@gmail.com>
2022-08-01 11:21:41 +0530

    Random Color Generator

---
+++ block-BJaaek/code/script.js
@@ -0,0 +1,9 @@
+let firstBox = document.querySelector('.first');
+firstBox.addEventListener('click', function () {
+    firstBox.style.backgroundColor = 'orange'
+}  );
+
+let secondBox = document.querySelector('.second');
+secondBox.addEventListener('mousemove', function () {
+    secondBox.style.backgroundColor = "purple";
+}  );
