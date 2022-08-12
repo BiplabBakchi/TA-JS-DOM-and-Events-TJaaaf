4bd547d026d3354dc18c5d64a92a6c9cfbe8233c
Biplab Bakchi <biplabbakchi3@gmail.com>
2022-08-12 11:49:39 +0530

    Rock Paper Scissors Game

--- block-BJaaen/code/script.js
+++ block-BJaaen/code/script.js
@@ -1,45 +1,83 @@
-let playerPoint = document.querySelector('.player');
-let computerPoint = document.querySelector('.computer');
-let playerBtn = document.querySelectorAll
-('.manual');
+let userRoot = document.querySelector('.user-icons');
+let computerRoot = document.querySelector('.computer-icons');
 let result = document.querySelector('h3');
-let player;
-let computer;
+let resetBtn = document.querySelector('.reset');
 
-playerBtn.forEach(ele => {
-    ele.addEventListener('click', (event) => {
-       player = event.target.dataset.selection;
-        computerTurn();
+let dataSet = [
+  {
+    name: 'rock',
+    beats: 'scissors',
+  },
+  {
+    name: 'paper',
+    beats: 'rock',
+  },
+  {
+    name: 'scissors',
+    beats: 'paper',
+  },
+]
+
+let userSelected = {},
+ computerSelected = {};
+
+function getWinner(user, computer) {
+    if(user.name === computer.name) {
+        result.innerText = 'Draw';
+    }else if (user.beats === computer.name)  {
+        result.innerText = 'You Won';
+
+    }else {
+        result.innerText = 'You Lost';
+    }
+}
+
+function getRandomNumber(max=3) {
+   return Math.floor(Math.random() * max);
+}
+
+function createUserLayout () {
+    userRoot.innerHTML = '';
+    dataSet.forEach((icon)=> {
+    let li = document.createElement("li");
+    let i = document.createElement("i");
+    i.className = `fa fa-hand-${icon.name}-o`;
+    if(userSelected.name === icon.name) {
+        li.classList.add('selected')
+    }
+    li.addEventListener('click', ()=> {
+        userSelected = icon;
+        computerSelected = dataSet[getRandomNumber()];
+        getWinner(userSelected, computerSelected);
+        createComputerLayout();
+        createUserLayout();
+        console.log(userSelected, computerSelected);
     })
- 
-    result.innerText = checkwinner()
-
-})
-
-function computerTurn() {
-    let randomNum = Math.floor(Math.random() * 3) + 1;
-
-    switch(randomNum) {
-        case 1: 
-            computer = "rock";
-        break;
-        case 2: 
-            computer = "paper";
-        break;
-        case 3: 
-            computer = "scissors";
-        break;
-    }
+    li.append(i);
+    userRoot.append(li);
+    });
 }
+createUserLayout();
 
-function checkwinner() {
-    if (player == computer) {
-        return "Draw";
-    }else if (computer = "rock") {
-        return(player = "paper") ? 'You win' : 'You lose';
-    }else if (computer = "paper") {
-        return(player = "scissors") ? 'You win' : 'You lose';
-    }else if (computer = "scissors") {
-        return(player = "rock") ? 'You win' : 'You lose';
+function createComputerLayout () {
+    computerRoot.innerHTML = '';
+    dataSet.forEach((icon)=> {
+    let li = document.createElement("li");
+    let i = document.createElement("i");
+    i.className = `fa fa-hand-${icon.name}-o`;
+    if(computerSelected.name === icon.name) {
+        li.classList.add('selected')
     }
+    li.append(i);
+    computerRoot.append(li);
+    });
 }
+createComputerLayout();
+
+resetBtn.addEventListener('click',()=> {
+    userSelected = {};
+    computerSelected = {};
+    createUserLayout();
+    createComputerLayout();
+    result.innerText = '';
+});
