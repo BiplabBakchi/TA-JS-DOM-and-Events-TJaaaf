f8259466adff899b52179f4de06b40943dcab658
Biplab Bakchi <biplabbakchi3@gmail.com>
2022-08-16 17:05:06 +0530

    Handling Different Input Elements

--- block-BJaaer/code/script.js
+++ block-BJaaer/code/script.js
@@ -1,36 +1,41 @@
 let form = document.querySelector("form");
 let userInfo = {};
-
+let modal = document.querySelector('.modal');
+console.dir(modal);
+let close = document.querySelector('.close');
 function handleSubmit(event) {
     event.preventDefault();
-    userInfo.name = (form.elements.text.value);
+    userInfo.name = (form.elements.name.value);
     userInfo.email = (form.elements.email.value);
-    userInfo.gendre = (form.elements.gender.value);
+    userInfo.movies = (form.elements.gender.movies);
     userInfo.color =(form.elements.color.value);
-    userInfo.range =(form.elements[4].value);
+    userInfo.rating =(form.elements.range.value);
     userInfo.genre = (form.elements.drone.value);
     userInfo.terms = (form.elements.terms.checked);
     console.log(userInfo);
+
+    modal.style.display = "block";
+    form.style.display = "none";
+
+  // modal
+
+  close.addEventListener("click", function () {
+    form.style.display = "flex";
+    modal.style.display = "none";
+  });
+  form.reset();
+  modal.children[1].innerText = `Hello ${userInfo.name}`;
+  modal.children[2].innerText = `Email: ${userInfo.email}`;
+  modal.children[3].innerText = `You Love: ${userInfo.movies}`;
+  modal.children[4].innerText = `Color: ${userInfo.color}`;
+  modal.children[5].innerText = `Rating: ${userInfo.rating}`;
+  modal.children[6].innerText = `Book Genre: ${userInfo.genre}`;
+  if (userInfo.terms == true) {
+    modal.children[7].innerText = `✅ You agreed to Terms and conditions`;
+  } else {
+    modal.children[7].innerText = `❌ You didn't agree to Terms and conditions`;
+  }
 };
 form.addEventListener('submit', handleSubmit);
 
-/*
-<h1>Hello Name</h1>
-      <h2>Email: exp@gmail.com</h2>
-      <h2>You LOve: Movies</h2>
-      <h2>Color: #ffffff</h2>
-      <h2>8</h2>
-      <h2>Book: Fiction #fffff</h2>
-      <h2>You agreed to terms and  condition</h2>
-*/
-/*
-let rootElement = document.querySelector('.model')
-let h2 = document.querySelector('h1');
-function createLayout(userInfo) {
-    userInfo.forEach((info) => {
-        document.createElement
-    
-    })
-}
-*/
-
+console.log(userInfo);
